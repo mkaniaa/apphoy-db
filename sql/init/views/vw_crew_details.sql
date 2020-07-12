d6b7dac0b93ca6f99cfb32a2ebfcd58862aba7d2
mkaniaa <mkaniaa@gmail.com>
2020-07-12 20:42:04 +0200

    corrections in crew views

--- sql/init/views/vw_crew_details.sql
+++ sql/init/views/vw_crew_details.sql
@@ -15,15 +15,15 @@ CREATE OR REPLACE VIEW vw_crew_details AS
         sk.cruise_common_name,
         sk.boat_common_name,
         sk.skipper_full_name,
+        p.id AS participant_id,
         p.name || ' ' || p.surname AS crew_member_full_name,
         DATE_PART('year', AGE(p.birth_date)) AS crew_member_age,
-        CONCAT(
-            (ROW_NUMBER() OVER(PARTITION BY sk.cruise_id, sk.boat_id ORDER BY sk.boat_id, sk.skipper_full_name, p.id))::TEXT,
-            '/', b.bunks_number) AS member_count
+        ROW_NUMBER() OVER(PARTITION BY sk.cruise_id, sk.boat_id ORDER BY sk.boat_id, sk.skipper_full_name, p.id) AS member_number,
+        b.bunks_number AS boat_bunks_number
     FROM crew cw
     JOIN participant p ON cw.participant_id = p.id
     JOIN vw_boat_skipper sk ON cw.boat_id = sk.boat_id AND cw.cruise_id = sk.cruise_id
     JOIN boat b ON cw.boat_id = b.id
-    ORDER BY sk.cruise_start_date, sk.boat_common_name, sk.skipper_full_name, member_count;
+    ORDER BY sk.cruise_start_date, sk.boat_common_name, sk.skipper_full_name, member_number;
 
 COMMENT ON VIEW vw_boat_skipper IS 'Script creates view for details about all crews.';
